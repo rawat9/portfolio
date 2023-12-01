a50a5f9c956455d8bc7f28929805ce921e414b19
Anurag Rawat <anurag.rawat0201@gmail.com>
2023-12-01 15:10:03 +0000

    customize the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,14 @@
 <footer class="mb-10 px-4 text-center text-gray-500">
-  <small class="mb-2 block text-xs"> &copy; 2023 Anurag Rawat. All rights reserved. </small>
-  <p class="text-xs">Built with SvelteKit, TypeScript, Tailwind CSS, Vercel hosting.</p>
+  <p class="text-xs">
+    Built with <a href="https://kit.svelte.dev/" class="font-bold underline underline-offset-2"
+      >SvelteKit</a
+    >,
+    <a href="https://www.typescriptlang.org/" class="font-bold underline underline-offset-2"
+      >TypeScript</a
+    >, styled using
+    <a href="https://tailwindcss.com/" class="font-bold underline underline-offset-2"
+      >Tailwind CSS</a
+    >, hosted on
+    <a href="https://vercel.com/" class="font-bold underline underline-offset-2">Vercel</a>.
+  </p>
 </footer>
